57642957ccdf64f145b2871b3aae9f962035e5e5
ChenglongSun <cs4543@nyu.edu>
2017-09-27 15:53:58 -0400

    Sep 27

--- integer.java
+++ integer.java
@@ -1,18 +1,26 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        @SuppressWarnings("resource")
-        Scanner scanner=new Scanner(System.in);
-        while(scanner.hasNext()){
-            String s=scanner.nextLine();
-            int len=s.length();
-            int []arr1=new int[10];
-            for(int i=len-1;i>=0;i--){
-                if(arr1[s.charAt(i)-48]==0){
-                    System.out.print(s.charAt(i)-48);
-                    arr1[s.charAt(i)-48]++;
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        List<Character>[] bucket = new List[s.length() + 1];
+        for (char x: map.keySet()) {
+            int i = map.get(x);
+            if (bucket[i] == null) bucket[i] = new ArrayList<Character>();
+            bucket[i].add(x);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (char key : bucket[i]) {
+                    for (int j = 0; j < map.get(key); j++) {
+                        sb.append(key);
+                    }
                 }
             }
         }
+        return sb.toString();
     }
 }
