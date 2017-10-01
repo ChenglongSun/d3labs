596b4e424a96a8449abe25b7ab271247c3399e3c
ChenglongSun <cs4543@nyu.edu>
2017-09-30 23:26:38 -0400

    Sep 30

---
+++ findk.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<String>();
+        for (String s : wordList) {
+            dict.add(s);
+        }
+        
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(beginWord);
+        queue.add(null);//
+        Set<String> visited = new HashSet<String>();
+        visited.add(beginWord);
+        int len = 1;
+        
+        while (!queue.isEmpty()) {
+            String str = queue.poll();
+            if (str != null) {
+                for (int i = 0; i < str.length(); i++) {
+                    char[] chars = str.toCharArray();
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        chars[i] = j;
+                        String word = new String(chars);
+                        if (word.equals(endWord) && dict.contains(word)) return len + 1;
+                        if (!visited.contains(word) && dict.contains(word)) {
+                            visited.add(word);
+                            queue.add(word);
+                        }
+                    }
+                }
+            } else {
+                len++;
+                if (!queue.isEmpty()) {
+                    queue.add(null);
+                }
+            }
+        }
+        return 0;
+    }
+}
