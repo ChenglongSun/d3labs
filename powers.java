a427f1d0f22d12f986cbc8ce5abe98afaeb530fb
ChenglongSun <cs4543@nyu.edu>
2017-10-05 23:50:42 -0400

    Oct 5

---
+++ powers.java
@@ -0,0 +1,34 @@
+package com.test1;
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+        int nums[] = {1,2,3,2,2,5};
+        
+        int res = subst(nums);
+        System.out.println(res);
+    }
+	
+	public static int subst(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int count = 0;
+		int sum = 0;
+		map.put(0, 1);
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			int key = sum % 3;
+			sum = key;
+			map.put(key, map.getOrDefault(key, 0) + 1);
+		}
+		for (int i : map.keySet()) {
+			int times = map.get(i) - 1;
+			if (times > 0) {
+				if (times == 1) {
+					count += 1;
+				} else {
+					count += Math.pow(2, times) - 1;
+				}
+			}
+		}
+		return count;
+	}
+}
