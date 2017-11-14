fb26df3d312c3e9a881e45a215d42a0391e6b718
ChenglongSun <cs4543@nyu.edu>
2017-11-14 17:46:53 -0500

    Nov 14

--- color game/colorgame.js
+++ color game/colorgame.js
@@ -1,14 +1,34 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)"
-]
+var colors = generateRandomColor(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#ezbtn");
+var hardButton = document.querySelector("#hdbtn");
+
+easyButton.addEventListener("click", function(){
+	easyButton.classList.add("selected");
+	hardButton.classList.remove("selected");
+})
+
+hardButton.addEventListener("click", function(){
+	hardButton.classList.add("selected");
+	easyButton.classList.remove("selected");
+})
+
+resetButton.addEventListener("click", function(){
+	colors = generateRandomColor(6);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i < colors.length; i++) {
+		squares[i].style.background = colors[i];
+	}
+
+})
+
+
 colorDisplay.textContent = pickedColor;
 
 
@@ -18,9 +38,47 @@ for (var i = 0; i < squares.length; i++) {
 	//listener
 	squares[i].addEventListener("click", function() {
 		if (this.style.background == pickedColor) {
-			alert("right!!!");
+			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "play again";
+			changeColors(pickedColor);
+			h1.style.background = pickedColor;
 		} else {
-			alert("wrong!!!");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	})
 }
+
+function changeColors(color) {
+	for (var i = 0; i < colors.length; i++) {
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+
+function generateRandomColor(num) {
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+
+
+
+
+
+
