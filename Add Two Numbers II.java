3a662c25fe5084effda2ff08a2b333904583bf57
ChenglongSun <cs4543@nyu.edu>
2017-12-22 23:34:59 -0500

    Dec 22

--- Add Two Numbers II.java
+++ Add Two Numbers II.java
@@ -2,8 +2,7 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         Stack<Integer> s1 = new Stack<>();
         Stack<Integer> s2 = new Stack<>();
-        
-        
+                
         while (l1 != null) { 
             s1.push(l1.val);
             l1 = l1.next;
