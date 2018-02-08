497eb2d09c6a8fa5b8ec133c5715e8fa2d09d076
ChenglongSun <cs4543@nyu.edu>
2018-02-07 20:30:29 -0500

    Feb 7

--- s.java
+++ s.java
@@ -1,28 +1,15 @@
 class Solution {
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) return res;
-        boolean[] used = new boolean[nums.length];
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
-        dfs(nums, used, res, list);
-        return res;
-        
-    }
-    
-    public void dfs(int[] nums, boolean[] used, List<List<Integer>> res, List<Integer> temp) {
-        if (temp.size() == nums.length) {
-            res.add(new ArrayList<Integer>(temp));
-            return;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int[] row : obstacleGrid) {
+            for (int i = 0; i < n; i++) {
+                if (row[i] == 1) dp[i] = 0;
+                else if (i > 0) dp[i] += dp[i - 1];
+            }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-            if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) continue;
-            used[i] = true;
-            temp.add(nums[i]);
-            dfs(nums, used, res, temp);
-            used[i] = false;
-            temp.remove(temp.size() - 1);
-        }
+        return dp[n - 1];
     }
 }
