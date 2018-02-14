4f88dc3c32af461cbb45021033a642145f8a8781
ChenglongSun <cs4543@nyu.edu>
2018-02-13 22:45:48 -0500

    Feb 13

--- s.java
+++ s.java
@@ -1,13 +1,38 @@
-public String simplifyPath(String path) {
-    Deque<String> stack = new LinkedList<>();
-    Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
-
-    for (String dir : path.split("/")) {
-        if (dir.equals("..") && !stack.isEmpty()) stack.pop();
-        else if (!skip.contains(dir)) stack.push(dir);
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] raw = new int[128];
+        int[] target = new int[128];
+        
+        for (int i = 0; i < t.length(); i++) {
+            target[t.charAt(i)]++;
+        }
+        int min = Integer.MAX_VALUE;
+        String res = new String();
+        int count = 0, start = 0, end = 0;
+        
+        while (end < s.length()) {
+            if (target[s.charAt(end)] != 0) {
+                if (raw[s.charAt(end)] < target[s.charAt(end)]) {
+                    count++;
+                }
+                raw[s.charAt(end)]++;
+            }
+            
+            if (count == t.length()) {
+                while (target[s.charAt(start)] == 0 || raw[s.charAt(start)] > target[s.charAt(start)]) {
+                    if (raw[s.charAt(start)] > target[s.charAt(start)]) {
+                        raw[s.charAt(start)]--;
+                    }
+                    start++;
+                }
+                
+                if (end - start + 1 < min) {
+                    min = end - start + 1;
+                    res = s.substring(start, end + 1);
+                }
+            }
+            end++;
+        }
+        return res;
     }
-
-    String res = "";
-    for (String dir : stack ) res = "/" + dir + res;
-    return res.isEmpty() ? "/" : res;
 }
