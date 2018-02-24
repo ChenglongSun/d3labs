479b129b1eb67591bb390802381fdabde4850302
ChenglongSun <cs4543@nyu.edu>
2018-02-23 20:14:25 -0500

    Feb 23

--- s.java
+++ s.java
@@ -1,18 +1,17 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (nums1 == null || nums2 == null) return;
-        int i = m - 1, j = n - 1, len = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] >= nums2[j]) {
-                nums1[len--] = nums1[i--];
-            } else {
-                nums1[len--] = nums2[j--];
-            }
-        }
-        if (j >= 0) {
-            for (; j >= 0; j--) {
-                nums1[j] = nums2[j];
-            }
-        }
+public List<String> restoreIpAddresses(String s) {
+    List<String> solutions = new ArrayList<String>();
+    restoreIp(s, solutions, 0, "", 0);
+    return solutions;
+}
+
+private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+    if (count > 4) return;
+    if (count == 4 && idx == ip.length()) solutions.add(restored);
+    
+    for (int i=1; i<4; i++) {
+        if (idx+i > ip.length()) break;
+        String s = ip.substring(idx,idx+i);
+        if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
+        restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
     }
 }
