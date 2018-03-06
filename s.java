52ec3422ac6a02875a9dd54c8a1fc7d942e7e9c8
ChenglongSun <cs4543@nyu.edu>
2018-03-05 21:15:47 -0500

    Mar 5

--- s.java
+++ s.java
@@ -1,44 +1,19 @@
-public class BSTIterator {
-    private Stack<TreeNode> stack;
-    public BSTIterator(TreeNode root) {
-        stack = new Stack<TreeNode>();
-        TreeNode curr = root;
-        while(curr != null) {
-            stack.push(curr);
-            if (curr.left != null) {
-                curr = curr.left;
-            } else {
-                break;
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s == null || s.length() <= 1) return true;
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        for(int i = 0 ; i< s.length(); i++){
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a)){
+                 if(map.get(a).equals(b)) continue;
+                else return false;
+            }else{
+                if(!map.containsValue(b)) map.put(a,b);
+                else return false;
             }
         }
-    }
-
-    /** @return whether we have a next smallest number */
-    public boolean hasNext() {
-        return !stack.isEmpty();
-    }
-
-    /** @return the next smallest number */
-    public int next() {
-        TreeNode node = stack.pop();
-        TreeNode cur = node;
-        if (cur.right != null) {
-            cur = cur.right;
-            while (cur != null) {
-                stack.push(cur);
-                if(cur.left != null) {
-                    cur = cur.left;
-                } else {
-                    break;
-                }
-            }
-        }
-        return node.val;
+        return true;
+        
     }
 }
-
-/**
- * Your BSTIterator will be called like this:
- * BSTIterator i = new BSTIterator(root);
- * while (i.hasNext()) v[f()] = i.next();
- */
