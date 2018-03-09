d8237e0320fd9d807c1cc367144fa46779edd0b9
ChenglongSun <cs4543@nyu.edu>
2018-03-08 23:45:20 -0500

    Mar 8

--- s.java
+++ s.java
@@ -1,16 +1,33 @@
- public int kthSmallest(TreeNode root, int k) {
-        int count = countNodes(root.left);
-        if (k <= count) {
-            return kthSmallest(root.left, k);
-        } else if (k > count + 1) {
-            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> ret = new LinkedList<Integer>();
+        for (int i=0; i<input.length(); i++) {
+            if (input.charAt(i) == '-' ||
+                input.charAt(i) == '*' ||
+                input.charAt(i) == '+' ) {
+                String part1 = input.substring(0, i);
+                String part2 = input.substring(i+1);
+                List<Integer> part1Ret = diffWaysToCompute(part1);
+                List<Integer> part2Ret = diffWaysToCompute(part2);
+                for (Integer p1 :   part1Ret) {
+                    for (Integer p2 :   part2Ret) {
+                        int c = 0;
+                        switch (input.charAt(i)) {
+                            case '+': c = p1+p2;
+                                break;
+                            case '-': c = p1-p2;
+                                break;
+                            case '*': c = p1*p2;
+                                break;
+                        }
+                        ret.add(c);
+                    }
+                }
+            }
         }
-        
-        return root.val;
+        if (ret.size() == 0) {
+            ret.add(Integer.valueOf(input));
+        }
+        return ret;
     }
-    
-    public int countNodes(TreeNode n) {
-        if (n == null) return 0;
-        
-        return 1 + countNodes(n.left) + countNodes(n.right);
-    }
+}
