073b53cb02ab16a21b820c60bca5a0107249877b
ChenglongSun <cs4543@nyu.edu>
2018-03-13 23:23:44 -0400

    Mar 13

--- s.java
+++ s.java
@@ -1,9 +1,18 @@
-SELECT A.Name from Customers A
-WHERE NOT EXISTS (SELECT 1 FROM Orders B WHERE A.Id = B.CustomerId)
+class NumArray {
+    int[] sum;
+    public NumArray(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] += nums[i];
+        }        
+    }
+    
+    public int sumRange(int i, int j) {
+        return nums[j + 1] - nums[i];
+    }
+}
 
-SELECT A.Name from Customers A
-LEFT JOIN Orders B on  a.Id = B.CustomerId
-WHERE b.CustomerId is NULL
-
-SELECT A.Name from Customers A
-WHERE A.Id NOT IN (SELECT B.CustomerId from Orders B
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
