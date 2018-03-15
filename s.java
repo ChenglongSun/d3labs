885d26a76ec488f16ead3ffe66fc3056105fcad2
ChenglongSun <cs4543@nyu.edu>
2018-03-14 23:11:57 -0400

    Mar 13

--- s.java
+++ s.java
@@ -1,18 +1,25 @@
-class NumArray {
-    int[] sum;
-    public NumArray(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            sum[i] += nums[i];
-        }        
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] root = new int[n];
+        for (int i = 0; i < n; i++) root[i] = i;
+        
+        for (int[] edge : edges) {
+            int root1 = findRoot(root, edge[0]);
+            int root2 = findRoot(root, edge[1]);
+            
+            if (root1 != root2) {
+                root[root1] = root2;
+                n--;
+            }
+        }
+        return n;
     }
     
-    public int sumRange(int i, int j) {
-        return nums[j + 1] - nums[i];
+    public int findRoot(int[] root, int id) {
+        while (root[id] != id) {
+            root[id] = root[root[id]];// 加速
+            id = root[id];
+        }
+        return id;
     }
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(i,j);
- */
