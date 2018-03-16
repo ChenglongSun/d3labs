c0502358ad3a8f2cf19a3b2de42e76e82902cdcc
ChenglongSun <cs4543@nyu.edu>
2018-03-15 23:50:25 -0400

    Mar 15

--- s.java
+++ s.java
@@ -1,25 +1,39 @@
-class Solution {
-    public int countComponents(int n, int[][] edges) {
-        int[] root = new int[n];
-        for (int i = 0; i < n; i++) root[i] = i;
-        
-        for (int[] edge : edges) {
-            int root1 = findRoot(root, edge[0]);
-            int root2 = findRoot(root, edge[1]);
-            
-            if (root1 != root2) {
-                root[root1] = root2;
-                n--;
+public class Solution {
+    public boolean isValid(String code) {
+        Stack<String> stack = new Stack<>();
+        for(int i = 0; i < code.length();){
+
+            if(i>0 && stack.isEmpty()) return false;
+
+            if(code.startsWith("<![CDATA[", i)){//cdate
+                int j = i+9;
+                i = code.indexOf("]]>", j);
+                if(i < 0) return false;
+                i += 3;
+
+            }else if(code.startsWith("</", i)){ //tag close
+                int j = i + 2;
+                i = code.indexOf('>', j);
+                if(i < 0 || i == j || i - j > 9) return false;
+                for(int k = j; k < i; k++){
+                    if(!Character.isUpperCase(code.charAt(k))) return false;
+                }
+                String s = code.substring(j, i++);
+                if(stack.isEmpty() || !stack.pop().equals(s)) return false;//check stack
+
+            }else if(code.startsWith("<", i)){//tag open
+                int j = i + 1;
+                i = code.indexOf('>', j);
+                if(i < 0 || i == j || i - j > 9) return false;
+                for(int k = j; k < i; k++){
+                    if(!Character.isUpperCase(code.charAt(k))) return false;
+                }
+                String s = code.substring(j, i++);
+                stack.push(s);//store the tag
+            }else{
+                i++;
             }
         }
-        return n;
-    }
-    
-    public int findRoot(int[] root, int id) {
-        while (root[id] != id) {
-            root[id] = root[root[id]];// 加速
-            id = root[id];
-        }
-        return id;
+        return stack.isEmpty();
     }
 }
