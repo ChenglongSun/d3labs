6b00e230f772d3d1074f253cba8277e3b038a345
ChenglongSun <cs4543@nyu.edu>
2018-03-26 22:50:59 -0400

    Mar 26

--- file path.java
+++ file path.java
@@ -4,11 +4,15 @@ public int lengthLongestPath(String input) {
         int maxLen = 0;
         for(String s:input.split("\n")){
             int lev = s.lastIndexOf("\t")+1; // number of "\t"
+
             while(lev+1<stack.size()) stack.pop(); // find parent
+
             int len = stack.peek()+s.length()-lev+1; // remove "/t", add"/"
+
             stack.push(len);
             // check if it is file
             if(s.contains(".")) maxLen = Math.max(maxLen, len-1); 
         }
         return maxLen;
     }
+    
