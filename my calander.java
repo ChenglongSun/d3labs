2f3e4acbd2f6b4b677290e4f7feeaebdab78437a
ChenglongSun <cs4543@nyu.edu>
2018-05-06 23:41:47 -0400

    May 6

--- my calander.java
+++ my calander.java
@@ -10,7 +10,6 @@ class MyCalendar {
         if (floorKey != null && calendar.get(floorKey) > start) return false;
         Integer ceilingKey = calendar.ceilingKey(start);
         if (ceilingKey != null && ceilingKey < end) return false;
-
         calendar.put(start, end);
         return true;
     }
