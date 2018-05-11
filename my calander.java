9192681ab8fc9381f53ea1194fcf49cb0c5b3c0b
ChenglongSun <cs4543@nyu.edu>
2018-05-10 23:57:28 -0400

    May 10

--- my calander.java
+++ my calander.java
@@ -4,6 +4,7 @@ class MyCalendar {
     public MyCalendar() {
         calendar = new TreeMap<>();
     }
+    
     public boolean book(int start, int end) {
         Integer floorKey = calendar.floorKey(start);
         if (floorKey != null && calendar.get(floorKey) > start) return false;
