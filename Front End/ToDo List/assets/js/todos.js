f9410324fbdb1ba3f82628d280295ffad44913a2
ChenglongSun <cs4543@nyu.edu>
2018-05-17 22:49:31 -0400

    May 17

---
+++ Front End/ToDo List/assets/js/todos.js
@@ -0,0 +1,22 @@
+//click off todo
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");	
+});
+
+//click x to delete
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagration();
+});
+
+$("input[type = 'text']").keypress(function(event){
+	if (event.which === 13) {
+		//grabbing new to do task
+		var toDoText = $(this).val();
+		//create a new li
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " +toDoText+ "</li>");
+	}
+});
