de2589c0ea0c11df7fdc53b57f68c2b7ae34b346
ChenglongSun <cs4543@nyu.edu>
2018-05-21 23:14:02 -0400

    May 21

--- longest univalue path.java
+++ longest univalue path.java
@@ -1,16 +1,15 @@
 class Solution {
+    int res = 0;
     public int longestUnivaluePath(TreeNode root) {
-        int[] res = new int[1];
-        if (root != null) dfs(root, res);
-        return res[0];
+        if (root != null) dfs(root);
+        return res;
     }
-
-    private int dfs(TreeNode node, int[] res) {
-        int l = node.left != null ? dfs(node.left, res) : 0; // Longest-Univalue-Path-Start-At - left child
-        int r = node.right != null ? dfs(node.right, res) : 0; // Longest-Univalue-Path-Start-At - right child
-        int resl = node.left != null && node.left.val == node.val ? l + 1 : 0; // Longest-Univalue-Path-Start-At - node, and go left
-        int resr = node.right != null && node.right.val == node.val ? r + 1 : 0; // Longest-Univalue-Path-Start-At - node, and go right
-        res[0] = Math.max(res[0], resl + resr); // Longest-Univalue-Path-Across - node
+    private int dfs(TreeNode node) {
+        int l = node.left != null ? dfs(node.left) : 0; // Longest-Univalue-Path-Start-At - left child
+        int r = node.right != null ? dfs(node.right) : 0; // Longest-Univalue-Path-Start-At - right child
+        int resl = node.left != null && node.left.val == node.val ? l + 1 : 0; 
+        int resr = node.right != null && node.right.val == node.val ? r + 1 : 0; 
+        res = Math.max(res, resl + resr); // Longest-Univalue-Path-Across - node
         return Math.max(resl, resr);
     }
 }
